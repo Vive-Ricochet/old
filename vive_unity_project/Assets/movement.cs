39096a2ebdc3cccd438b8198ae3d3687401de0c8
Donovan <dnab0012@gmail.com>
2016-01-12 14:24:48 -0800

    added project
    added main project and scene; cube has movement

---
+++ vive_unity_project/Assets/movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	void Start (){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveX, 0.0f, moveZ);
+		rb.velocity = movement * speed;
+	}
+}
