116e23f083342ec56d0a645f49fe43a4c9b8d8fa
Chaiz <ctuimolo@ucsc.edu>
2016-01-14 16:19:09 -0800

    Attempted adding camera

---
+++ vive_unity_project/Assets/LookAtCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 offset;
+
+    // Called once at insertion into scene
+    void Start () {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Late update called after update has finished so player script calculates position first
+    void LateUpdate () {
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        transform.LookAt(target.transform);
+    }
+}
