b3b8cfc20a003da44a07f52c581447f2839aa319
Chaiz <ctuimolo@ucsc.edu>
2016-01-15 04:59:15 -0800

    Attempting to push CameraController and updated PlayerManager
    Hopefully this doesn't screw everything up...

---
+++ vive_unity_project/Assets/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    // private fields editable by inspector
+    [SerializeField] private Transform target;              // the target object for this camera
+    [SerializeField] private float verticalOffset = 1.7f;   // how high above the target the camera sits
+    [SerializeField] private float distance = 6.0f;         // distance from target
+    [SerializeField] private float xSpeed = 200.0f;         // camera X/Y sensitivity
+    [SerializeField] private float ySpeed = 200.0f;
+
+    // private fields
+    private float yMinLimit = -18.0f;   // lower and upper bounds of up/down rotation
+    private float yMaxLimit = 80.0f;
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+
+    void LateUpdate() {
+
+        if (!target)
+            return;
+
+        // read axis inputs from something (currently the mouse)
+        xDeg += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        yDeg -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+        yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit); // Keep up-down rotation within bounds
+
+        // camera rotation
+        Quaternion rotation = Quaternion.Euler(yDeg, xDeg, 0);
+
+        // camera position
+        Vector3 vTargetOffset = new Vector3(0, -verticalOffset, 0);
+        Vector3 position = target.position - (rotation * Vector3.forward * distance + vTargetOffset);
+
+        // apply transformations
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    // Mathf.Clamp functionality floor'd within 360 degree angles
+    static float ClampAngle(float angle, float min, float max) {
+        if (angle < -360f)
+            angle += 360f;
+        if (angle > 360f)
+            angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
--- vive_unity_project/Assets/PlayerManager.cs
+++ vive_unity_project/Assets/PlayerManager.cs
@@ -2,24 +2,74 @@
 using System.Collections;
 
 public class PlayerManager : MonoBehaviour {
-	private Rigidbody rb;
-	public float speed;
-	public bool grounded;
-	void Start (){
-		rb = GetComponent<Rigidbody> ();
-	}
-	void FixedUpdate(){
-		float moveX = Input.GetAxis ("Horizontal");
-		float moveZ = Input.GetAxis ("Vertical");
-		if ((Input.GetAxis ("Jump") == 1) && grounded) {
-			grounded = false;
-			rb.velocity  = new Vector3 (0.0f, Input.GetAxis("Jump") * speed, 0.0f);
-		}
-		rb.MovePosition(new Vector3 (rb.position.x + moveX, rb.position.y, rb.position.z + moveZ));
-		rb.useGravity = true;
-	}
 
+    // private fields editable by inspector
+    [SerializeField] private float speed = 20;  // player movement speed
+    [SerializeField] private float jump  = 20;   // player intitial jump velocity
+
+
+    // private fields
+	private bool grounded;
+    private Rigidbody rb;
+	
+    void Start (){
+		
+        // rigid body used for in-engine physics 
+        rb = GetComponent<Rigidbody> ();
+
+        // gravity is intended to be used for this object
+        rb.useGravity = true;
+        Physics.gravity = new Vector3(0f, -10.0f, 0f);
+	
+    }
+	
+    void FixedUpdate() {
+
+        // get normalized camera direction. Ignor Y transformation
+        Vector3 camDir = Camera.main.transform.forward;
+        camDir.y = 0;
+        camDir.Normalize();
+
+        // get camera's righthand direction for rotating up/down
+        Vector3 camRight = new Vector3(camDir.z, 0f, -camDir.x);
+
+        // apply input to relative movement velocity vector
+        Vector3 newVel = Vector3.zero;
+        newVel += Input.GetAxis("Horizontal") * camRight * speed;
+        newVel += Input.GetAxis("Vertical") * camDir * speed;
+
+        // check input for "Jump" and execute if grounded
+        if ((Input.GetAxis("Jump") == 1) && grounded)
+            // shoot velocity upwards. 
+            newVel += new Vector3(0.0f, jump, 0.0f);
+
+        // apply new velocity
+        rb.velocity = newVel;
+
+        /***** Old movement management based on position transform rather than velocity. *****
+          
+         
+        // read input from some controller
+        float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+        
+        // check input for "Jump" and execute if grounded
+		if ((Input.GetAxis ("Jump") == 1) && grounded)
+            // shoot velocity upwards. 
+			rb.velocity  = new Vector3 (0.0f, jump, 0.0f);
+
+        // update player position
+	    rb.MovePosition(new Vector3 (rb.position.x + moveX, rb.position.y, rb.position.z + moveZ));
+        */
+    }
+
+    // on 'entering' a collision: grounded = true
 	void OnCollisionEnter(Collision collisionInfo){
 		grounded = true;
 	}
+
+    // on 'exiting' a collision: grounded = false
+    void OnCollisionExit(Collision collisionInfo) {
+        grounded = false;
+    }
 }
